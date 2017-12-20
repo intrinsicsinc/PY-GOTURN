70c703b76a8ae77b075a98b7cd2546091f4be3ba
Eric Cheng <echengster@gmail.com>
2017-12-20 15:43:02 -0800

    Updated config paths.

--- show_tracker_vot.sh
+++ show_tracker_vot.sh
@@ -1,6 +1,6 @@
 DEPLOY_PROTO='./nets/tracker.prototxt'		 
-CAFFE_MODEL='./nets/_iter_100000.caffemodel'		
-TEST_DATA_PATH='/media/nrupatunga/STUDIES&SOFTWARES/Work-2017/DeepLearning/Datasets/VOT/vot2014'		
+CAFFE_MODEL='./nets/tracker.caffemodel'
+TEST_DATA_PATH='/home/echeng/intrinsics/analyses/2017-12-20-goturn/vot2014'
 
 python -m goturn.test.show_tracker_vot \
 	--p $DEPLOY_PROTO \
